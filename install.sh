cf94b91f4a52c0ef150b8b77a9c2f7fea7413461
michael <mikesz@fastmail.net>
2021-01-22 16:33:41 -0500

    asdf

--- install.sh
+++ install.sh
@@ -39,4 +39,5 @@ wget -q https://github.com/mikeszila/smartsoundsync/archive/main.zip -O ./main.z
 unzip -o main.zip -d smartsoundsync
 rm main.zip
 cd smartsoundsync
+echo "starting"
 node ./install-setup.js
