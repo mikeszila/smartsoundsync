e141a8ba32830137d6d060c6df69d2d4788b814e
michael <mikesz@fastmail.net>
2021-01-23 21:47:05 -0500

    hello

--- hwtobuffer.js
+++ hwtobuffer.js
@@ -107,7 +107,7 @@ function spawnpcmRecord() {
 
             let sendTime = htstamp - (delay * sampleTimeMS) + source_buffer_time
 
-            console.log(audioData.length / 4, 'read', read, 'sampleIndex', sampleIndex, 'triggerhtstamp', triggerhtstamp, 'sendTime', sendTime, 'diff', sendTime - sendTimeLast, 'avail', avail, 'delay', delay, 'state', state)
+            //console.log(audioData.length / 4, 'read', read, 'sampleIndex', sampleIndex, 'triggerhtstamp', triggerhtstamp, 'sendTime', sendTime, 'diff', sendTime - sendTimeLast, 'avail', avail, 'delay', delay, 'state', state)
 
             let volumeLeft = audioData.readInt16LE(0)
             let volumeRight = audioData.readInt16LE(2)
